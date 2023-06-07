5cd8c95bfcaf50a6f990f80bde0ad371ed3c98a2
admin <admin@DESKTOP-DPEV3K1>
2023-06-07 20:30:33 +0200

    Add camera feature.

--- SPW/MainCamera.cpp
+++ SPW/MainCamera.cpp
@@ -18,12 +18,26 @@ void MainCamera::Update()
 {
     LevelScene &levelScene = dynamic_cast<LevelScene &>(m_scene);
     PE_Body *playerBody = levelScene.GetPlayer()->GetBody();
+    Camera *camera = m_scene.GetActiveCamera();
+    Creative * creative;
 
     m_center.SetTarget(playerBody->GetPosition());
     m_center.Update(levelScene.GetTime());
 
-    PE_Vec2 shift(-3.0f, 0.0f);
-    TranslateWorldView(m_center - m_worldView.GetCenter() - shift);
+    PE_Vec2 startPosition(levelScene.GetStartingPoint().x + 11.0f, m_center.y);
+
+    PE_Vec2 cameraPos = m_center;
+
+    if (camera->GetWorldView().lower.x <= 0 && m_center.x < camera->GetWorldView().upper.x / 2)
+    {
+        cameraPos = startPosition;
+    }
+    else if (m_center.x > camera->GetWorldView().upper.x / 2)
+    {
+        cameraPos = m_center;
+    }
+    
+    TranslateWorldView(cameraPos - m_worldView.GetCenter());
 
     // Fixe la position verticale minimale de la caméra pour ne pas voir sous le sol
     float dispLowerY = m_worldBounds.lower.y - m_worldView.lower.y;
